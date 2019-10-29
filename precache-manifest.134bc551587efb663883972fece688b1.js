82c7336efb039e22dc12851f12161f5930364448
alexviarhey <lexa.vergey@gmail.com>
2019-10-29 18:14:02 +0300

    Updates

---
+++ precache-manifest.134bc551587efb663883972fece688b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae41c215f8300b6e1451e35a1d4257d",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "cc146bc1c4bf741d1925",
+    "url": "/calculator/static/css/main.01753b5b.chunk.css"
+  },
+  {
+    "revision": "2aafcbb653661f00ebc9",
+    "url": "/calculator/static/js/2.e0a1c199.chunk.js"
+  },
+  {
+    "revision": "cc146bc1c4bf741d1925",
+    "url": "/calculator/static/js/main.255c9890.chunk.js"
+  },
+  {
+    "revision": "81eb61f5a8d26d2fd52d",
+    "url": "/calculator/static/js/runtime-main.1d26180e.js"
+  }
+]);
